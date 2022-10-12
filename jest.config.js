7f6a2081a2834bb6823f5bf7185b62735f4d89d8
waylon <waylong@wangxutech.com>
2022-10-12 17:18:39 +0800

    chore: jest 配置 alias

--- jest.config.js
+++ jest.config.js
@@ -8,6 +8,17 @@ module.exports = {
         //.ts文件用ts-jest处理
         '^.+\\.ts$': 'ts-jest'
     },
+    // 配置别名
+    moduleNameMapper: {
+        "@src/(.*)": "<rootDir>/src/$1",
+        "@assets/(.*)": "src/assets/$1",
+        "@api/(.*)": "src/api/$1",
+        "@pages/(.*)": "src/pages/$1",
+        "@router/(.*)": "src/router/$1",
+        "@store/(.*)": "src/store/$1",
+        "@components/(.*)": "src/components/$1",
+        "@styles/(.*)": "src/styles/$1",
+    },
     // 只执行 spec.js 结尾的文件
     testMatch: ['**/?(*.)+(spec).[jt]s?(x)']
 }
